cd13fed9e2284ac8d6367ee8222e97f52c457ab4
ARob110 <107314922+ARob110@users.noreply.github.com>
2023-07-22 16:30:57 -0400

    Update GameScene.swift

--- AvocadoRun/AvocadoRun/GameScene.swift
+++ AvocadoRun/AvocadoRun/GameScene.swift
@@ -1,24 +1,173 @@
 import SpriteKit
+import GameplayKit
 
-class MainMenuScene: SKScene {
-    let playButton = SKLabelNode(text: "Play")
+class GameScene: SKScene {
+    let player = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+    
+    var lastJumpTime: TimeInterval = 0
+    let jumpCooldown: TimeInterval = 0.6
+    
+    // Set up the ground variables
+    var groundTiles = [SKSpriteNode]()
+    var lastGroundTileX: CGFloat = 0.0
+    var lastGroundTileY: CGFloat = 0.0
+    let groundTileWidth: CGFloat = 100.0 // Adjust this based on your desired ground tile width
+    let maxPlatformHeight: CGFloat = 100.0 // Maximum height difference between platforms
+    
+    var gameOverLabel: SKLabelNode!
+    var isGameOver = false
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0
     
     override func didMove(to view: SKView) {
-        playButton.position = CGPoint(x: frame.midX, y: frame.midY)
-        addChild(playButton)
+        
+        let backgroundImg = SKSpriteNode(imageNamed: "Menubackground")
+        backgroundImg.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        backgroundImg.zPosition = -1
+        addChild(backgroundImg)
+        
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.zPosition = 1
+        
+        // Player physics
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.allowsRotation = false
+        
+        // Set the player's position
+        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+
+        addChild(player)
+
+        // Generate initial ground tiles
+        generateGround()
+        
+        // Generating game over
+        gameOverLabel = SKLabelNode(fontNamed: "Arial")
+        gameOverLabel.text = "Game Over! Tap to restart"
+        gameOverLabel.fontSize = 30
+        gameOverLabel.fontColor = SKColor.red
+        gameOverLabel.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        gameOverLabel.zPosition = 2
+        
+        // Score label
+        scoreLabel = SKLabelNode(fontNamed: "Arial")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontSize = 24
+        scoreLabel.fontColor = SKColor.black
+        scoreLabel.position = CGPoint(x: size.width / 2, y: size.height - scoreLabel.frame.size.height * 4)
+        scoreLabel.zPosition = 2
+        addChild(scoreLabel)
+        
+        startScoring()
+        print("Score Label: \(scoreLabel)")
+        print("Game Over Label: \(gameOverLabel)")
     }
     
+    func startScoring() {
+        let wait = SKAction.wait(forDuration: 1.0)
+        let block = SKAction.run({
+            self.score += 1
+            self.scoreLabel.text = "Score: \(self.score)"
+            print("Score: \(self.score)")
+        })
+        let sequence = SKAction.sequence([wait,block])
+        let repeatForever = SKAction.repeatForever(sequence)
+        run(repeatForever)
+    }
+    override func update(_ currentTime: TimeInterval) {
+        if player.position.y < 0 || player.position.y > size.height {
+            if !isGameOver {
+                gameOver()
+            }
+        } else {
+            moveGround()
+        }
+    }
+
+    
+    func gameOver() {
+        print("Game Over function called")
+        isGameOver = true
+        player.physicsBody?.isDynamic = false
+        addChild(gameOverLabel)
+        removeAllActions()
+    }
+
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let location = touch.location(in: self)
-            
-            if playButton.contains(location) {
-                if let view = self.view {
-                    let gameScene = GameScene(size: view.bounds.size)
-                    gameScene.scaleMode = .resizeFill
-                    let transition = SKTransition.flipHorizontal(withDuration: 0.5)
-                    view.presentScene(gameScene, transition: transition)
+            if isGameOver {
+                restartGame()
+            } else {
+                let currentTime = touches.first?.timestamp ?? 0
+                if currentTime - lastJumpTime >= jumpCooldown {
+                    if let dy = player.physicsBody?.velocity.dy {
+                        if dy < 0 {
+                            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 130))
+                        }
+                        else{
+                            player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 100))
+                        }
+                        lastJumpTime = currentTime
+                    }
                 }
+                
+            }
+        }
+    
+    func restartGame() {
+        gameOverLabel.removeFromParent()
+        isGameOver = false
+        player.physicsBody?.isDynamic = true
+        player.position = CGPoint(x: size.width * 0.5, y: size.height * 0.5)
+        lastGroundTileX = 0.0
+        lastGroundTileY = 0.0
+        groundTiles.forEach { $0.removeFromParent() }
+        groundTiles.removeAll()
+        generateGround()
+        startScoring()
+        
+        // Reset score
+        score = 0
+        scoreLabel.text = "Score: \(score)"
+    }
+    
+    func generateGround() {
+        // Generate ground tiles and add them
+        while lastGroundTileX < size.width {
+            let groundTile = SKSpriteNode(color: SKColor.brown, size: CGSize(width: groundTileWidth, height: 20))
+            let randomHeight = CGFloat.random(in: 0...maxPlatformHeight) // Platforms can only go upwards
+            groundTile.position = CGPoint(x: lastGroundTileX, y: lastGroundTileY + randomHeight)
+            groundTile.physicsBody = SKPhysicsBody(rectangleOf: groundTile.size)
+            groundTile.physicsBody?.isDynamic = false
+            addChild(groundTile)
+            groundTiles.append(groundTile)
+
+            lastGroundTileX += groundTileWidth
+            lastGroundTileY = groundTile.position.y
+        }
+    }
+
+    func moveGround() {
+        // Move the ground tiles to the left
+        for (index, groundTile) in groundTiles.enumerated() {
+            groundTile.position = CGPoint(x: groundTile.position.x - 2, y: groundTile.position.y)
+
+            // Check if the left edge of the ground tile is completely off the left edge of the screen
+            if groundTile.position.x - groundTileWidth / 2 < -size.width / 2 {
+                groundTile.removeFromParent()
+                groundTiles.remove(at: index)
+
+                // Add a new ground tile to the right
+                let newGroundTile = SKSpriteNode(color: SKColor.brown, size: CGSize(width: groundTileWidth, height: 20))
+                let randomHeight = CGFloat.random(in: -maxPlatformHeight...maxPlatformHeight)
+                newGroundTile.position = CGPoint(x: lastGroundTileX, y: lastGroundTileY + randomHeight)
+                newGroundTile.physicsBody = SKPhysicsBody(rectangleOf: newGroundTile.size)
+                newGroundTile.physicsBody?.isDynamic = false
+                addChild(newGroundTile)
+                groundTiles.append(newGroundTile)
+
+                lastGroundTileX += groundTileWidth
+                lastGroundTileY = newGroundTile.position.y
             }
         }
     }

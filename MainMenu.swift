85d6fd8f0dceb0681ad893a1410878122629d901
Jocelyn <63032964+Jberr10@users.noreply.github.com>
2023-07-23 01:33:22 -0400

    Updated MainMenu.swift

--- MainMenu.swift
+++ MainMenu.swift
@@ -2,55 +2,87 @@
 //  MainMenu.swift
 //  AvocadoRun
 //
-//  Created by Jocelyn Berrios on 7/21/23.
+//  Created by Jocelyn Berrios on 7/22/23.
 //
 
-import SwiftUI
-import UIKit
 import SpriteKit
-import GameplayKit
 
 class MainMenu: SKScene {
-    
-    let Menubackground = SKSpriteNode(imageNamed: "Menubackground")
+
+    //Declare Nodes
+    let backgroundImg = SKSpriteNode(imageNamed: "background")
+    let gameTitle = SKLabelNode(fontNamed: "Impact")
     let startButton = SKSpriteNode(imageNamed: "Start")
-    let settings = SKSpriteNode(imageNamed: "Settings")
+    let howToPlay = SKLabelNode(fontNamed: "Arial")
+    let how2 = SKLabelNode(fontNamed: "Arial")
+    let how3 = SKLabelNode(fontNamed: "Arial")
+    let playerImg = SKSpriteNode(imageNamed: "avocado")
     
-    
+    //Set the MainMenu Scene
     override func didMove(to view: SKView) {
         
-        // background image
-        addChild(Menubackground)
+        //Format and add Background  
+        backgroundImg.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        backgroundImg.zPosition = -1
+        addChild(backgroundImg)
         
-        //settings button
-        settings.position = CGPoint(x: 0, y: 100)
-        addChild(settings)
+        //Format and add gameTitle
+        gameTitle.text = "Avocado Run"
+        gameTitle.fontSize = 100
+        gameTitle.fontColor = SKColor.systemRed
+        gameTitle.position = CGPoint(x: frame.midX, y: self.size.height * 0.7)
+        gameTitle.zPosition = 1
+        addChild(gameTitle)
         
-        // Start button
-        startButton.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame))
+        //Format and add startButton
+        startButton.position = CGPoint(x: size.width * 0.5, y: self.size.height * 0.5)
+        startButton.zPosition = 1
         addChild(startButton)
         
+        //Format and add howToPlay title
+        howToPlay.text = "How to Play:"
+        howToPlay.fontSize = 30
+        howToPlay.fontColor = SKColor.darkGray
+        howToPlay.position = CGPoint(x: frame.midX, y: self.size.height*0.36)
+        howToPlay.zPosition = 1
+        addChild(howToPlay)
+
+        //Format and add howToPlay line 2 (how2) 
+        how2.text = "Tap the screen to jump"
+        how2.fontSize = 25
+        how2.fontColor = SKColor.darkGray
+        how2.position = CGPoint(x: frame.midX, y: self.size.height*0.33)
+        how2.zPosition = 1
+        addChild(how2)
+
+        //Format and add howToPlay line 3 (how3) 
+        how3.text = "Avoid falling to the ground"
+        how3.fontSize = 25
+        how3.fontColor = SKColor.darkGray
+        how3.position = CGPoint(x: frame.midX, y: self.size.height*0.31)
+        how3.zPosition = 1
+        addChild(how3)
         
+       //Format and add playerImg 
+        playerimg.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
+        playerimg.zPosition = 1
+        addChild(playerimg)
         
+    }//End of didMove
+    
+    // Create function for MainMenu touch behavior. When startButton is pressed, enter GameScene.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        
-        
-        // when user touches screen
-        //override func touchesBegan(_ touches: NSSet, with event: UIEvent) {
-        // for touch: AnyObject in touches {
-        
-        
-        
-        
-        
-        //}
-        
-        //
-        //override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        //}
-        
-        
-    }
-}
+        if let touch = touches.first {   
+            let location = touch.location(in: self)
+            
+            if startButton.contains(location) {
+                if let view = self.view { 
+                    let gameScene = GameScene(size: CGSize(width: 560, height: 1140))
+                    let transition = SKTransition.fade(withDuration: 0.50)
+                    view.presentScene(gameScene, transition: transition)
+                }
+            }
+        }
+    }//End of touchesBegan
+}//End of MainMenu
